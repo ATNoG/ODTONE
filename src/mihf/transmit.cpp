ATNoG/ODTONE

commit b983f6d3b764fd6a4bce7a0d3b7c560bd332f3d9

Fix source transaction state machine message sending loop
There was a possibility of the MIHF entering an infinite loop when
creating a new source transaction state machine.

src/mihf/transmit.cpp
@@ -14,6 +14,7 @@
 //
 
 #include <odtone/mihf/transmit.hpp>
+#include <odtone/mihf/transaction_manager.hpp>x
 #include <odtone/mihf/log.hpp>
 
 namespace odtone { namespace mihf {
@@ -27,7 +28,7 @@ transmit_t::transmit_t()
 transmit_t *transmit_t::instance()
 {
 	if (ptr_instance == NULL)
-        ptr_instance = new transmit_t();
+	   ptr_instance = new transmit_t();
 
 	return ptr_instance;
 }
@@ -53,12 +54,36 @@ void transmit_t::operator()(mih::message_ptr& msg)
 						}
 					else
 						{
-							netsap->send_to(msg, r->ip.c_str(), r->port);
+							tmanager->message_out(msg);
+							// netsap->send_to(msg, r->ip.c_str(), r->port);
 						}
 					return;
 				}
 		}
 }
+
+void transmit_t::send(mih::message_ptr& msg)
+{
+	std::set<registration_t>::iterator r;
+
+	for(r = _registrations.begin(); r != _registrations.end(); r++)
+	{
+		if(msg->destination().to_string().compare(r->name) == 0)
+		{
+			log(1, "(transmit) sending message to: ", r->name, " at ", r->ip, ":", r->port);
+			if ((r->ip.length() == 0) || (r->ip.compare( "127.0.0.1") == 0))
+			{
+				comhand->send_to(msg, "127.0.0.1", r->port);
+			}
+			else
+			{
+				netsap->send_to(msg, r->ip.c_str(), r->port);
+			}
+			return;
+		}
+	}
+}
+
 
 void transmit_t::add(mih::octet_string name, mih::octet_string ip, uint16 port)
 {

inc/odtone/mihf/transmit.hpp
@@ -30,48 +30,49 @@ namespace odtone { namespace mihf {
 
 struct registration_t
 {
-	mih::octet_string name;
-	uint16 port;
-	mih::octet_string ip;
+   mih::octet_string name;
+   uint16 port;
+   mih::octet_string ip;
 
-	bool operator<(const registration_t &other) const
-	{
-		if (name == other.name) {
-			if (ip == other.ip)
-				{
-					return false;
-				}
-			else
-				{
-					return (port < other.port);
-				}
-		}
+   bool operator<(const registration_t &other) const
+	  {
+		  if (name == other.name) {
+			  if (ip == other.ip)
+			  {
+				  return false;
+			  }
+			  else
+			  {
+				  return (port < other.port);
+			  }
+		  }
 
-		return (name < other.name);
-	}
+		  return (name < other.name);
+	  }
 };
 
 class transmit_t
 	: private boost::noncopyable
 {
 public:
-	static transmit_t* instance();
-	~transmit_t();
+   static transmit_t* instance();
+   ~transmit_t();
 
-	void operator()(mih::message_ptr& msg);
+   void operator()(mih::message_ptr& msg);
+   void send(mih::message_ptr& msg);
 
-	void add(mih::octet_string name, mih::octet_string ip, uint16 port);
+   void add(mih::octet_string name, mih::octet_string ip, uint16 port);
 
-	std::set<registration_t> _registrations;
+   std::set<registration_t> _registrations;
 
 protected:
-	transmit_t();
+   transmit_t();
 
-	static transmit_t *ptr_instance;
+   static transmit_t *ptr_instance;
 };
 
 #define transmit (*transmit_t::instance())
 
-  } /* namespace mihf */ } /* namespace odtone */
+} /* namespace mihf */ } /* namespace odtone */
 
 #endif

src/mihf/transaction_manager.cpp
@@ -314,7 +314,7 @@ void transaction_manager::run_transaction(dst_transaction_ptr t)
         t->start_ack_requestor  = t->out->ackreq();
         t->ack_requestor_status = ONGOING;
 
-        transmit(t->out);
+        transmit.send(t->out);
 
         // ??
         // if (t->start_ack_requestor)
@@ -384,7 +384,7 @@ void transaction_manager::run_transaction(src_transaction_ptr t)
 
 		log(5, t);
 
-        transmit(t->out);
+        transmit.send(t->out);
 
         if (t->opcode == mih::operation::response)
 			{

commit 9a7b5d8c656ba8a3d9609d4613b6331d9f6ee819

Fit typo introduced in commit b983f6d3b764fd6a4bce7a0d3b7c560bd332f3d9

src/mihf/transmit.cpp
@@ -14,7 +14,7 @@
 //
 
 #include <odtone/mihf/transmit.hpp>
-#include <odtone/mihf/transaction_manager.hpp>x
+#include <odtone/mihf/transaction_manager.hpp>
 #include <odtone/mihf/log.hpp>
 
 namespace odtone { namespace mihf {