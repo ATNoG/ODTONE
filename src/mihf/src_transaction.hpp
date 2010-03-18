ea513f9a8d291cdbe0b6a184746a5483a9f537ab
Simao Reis <sreis@av.it.pt>
2010-03-18 17:16:34 +0000

    [PATCH 4/6]: Add run() method to src_transaction_t class
    The run() method was in the transaction_manager because it was
    responsible for deleting the transaction when it finished. This
    functionality has been moved to another object (message_in,
    message_out).

--- src/mihf/src_transaction.hpp
+++ src/mihf/src_transaction.hpp
@@ -38,7 +38,7 @@ class src_transaction_t
 {
 public:
 	src_transaction_t();
-	void dump();
+	void run();
 
 	src_transaction_state_t state;
 };
@@ -47,7 +47,7 @@ public:
 typedef boost::shared_ptr< src_transaction_t > src_transaction_ptr;
 
 typedef std::set<src_transaction_ptr,
-				 transaction_compare<src_transaction_ptr > >src_transaction_set;
+		 transaction_compare<src_transaction_ptr > >src_transaction_set;
 
   } /* namespace mihf */
 } /* namespace odtone */
