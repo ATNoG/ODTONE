1c99b2ed7ed60f35b5875291cb7ec120f0a389c5
Simao Reis <sreis@av.it.pt>
2010-03-19 10:58:13 +0000

    Re-introduce net_sap class
    The net_sap class is responsible for sending messages to peer MIHFs.
    
    When the send() method is called it checks if the destination
    identifier is in the address book, if it is, proceeds to send the
    message.

---
+++ src/mihf/net_sap.cpp
@@ -0,0 +1,48 @@
+//
+// Copyright (c) 2007-2009 2009 Universidade Aveiro - Instituto de
+// Telecomunicacoes Polo Aveiro
+// This file is part of ODTONE - Open Dot Twenty One.
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//
+// Author:     Simao Reis <sreis@av.it.pt>
+//
+
+///////////////////////////////////////////////////////////////////////////////
+#include "net_sap.hpp"
+#include "address_book.hpp"
+#include "log.hpp"
+#include "utils.hpp"
+
+#include <boost/bind.hpp>
+///////////////////////////////////////////////////////////////////////////////
+
+namespace odtone { namespace mihf {
+
+net_sap::net_sap(io_service &io, address_book &abook)
+	: _io(io),
+	  _abook(abook)
+{
+}
+
+void net_sap::send(mih::message_ptr &msg)
+{
+	try {
+		address_entry a = _abook.get(msg->destination().to_string());
+
+		if (a.trans == mih::transport_udp)
+			utils::udp_send(_io, msg, a.ip.c_str(), a.port);
+		else
+			utils::tcp_send(_io, msg, a.ip.c_str(), a.port);
+	} catch(...) {
+		// TODO log;
+	}
+}
+
+} /* namespace mihf */ } /* namespace odtone */
+
