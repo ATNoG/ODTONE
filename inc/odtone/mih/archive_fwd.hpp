fc8663c601acce41a03aaf27e7468148cb59df92
Bruno Santos <bsantos@av.it.pt>
2010-04-16 20:02:15 +0100

    [mih]: Move the archive forward declarations to a separate header
    Using forward declarations allows to improve compile time.

---
+++ inc/odtone/mih/archive_fwd.hpp
@@ -0,0 +1,34 @@
+//=============================================================================
+// Brief   : Forward declarations for Archive
+// Authors : Bruno Santos <bsantos@av.it.pt>
+// ----------------------------------------------------------------------------
+// ODTONE - Open Dot Twenty One
+//
+// Copyright (C) 2010 Universidade de Aveiro
+// Copyrigth (C) 2010 Instituto de Telecomunicações - Pólo de Aveiro
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//=============================================================================
+
+#ifndef ODTONE_MIH_ARCHIVE_FWD__HPP_
+#define ODTONE_MIH_ARCHIVE_FWD__HPP_
+
+///////////////////////////////////////////////////////////////////////////////
+#include <odtone/base.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace odtone { namespace mih {
+
+class iarchive;
+class oarchive;
+
+///////////////////////////////////////////////////////////////////////////////
+} /* namespace mih */ } /*namespace odtone */
+
+// EOF ////////////////////////////////////////////////////////////////////////
+#endif /* ODTONE_MIH_ARCHIVE_FWD__HPP_ */
