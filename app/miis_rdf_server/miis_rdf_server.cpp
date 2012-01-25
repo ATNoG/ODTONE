4a111cb135f53a16546c989541728a57a4127ca9
Carlos Guimarães <cguimaraes@av.it.pt>
2012-01-25 11:14:10 +0000

    Updated headers

--- app/miis_rdf_server/miis_rdf_server.cpp
+++ app/miis_rdf_server/miis_rdf_server.cpp
@@ -1,11 +1,11 @@
 //=============================================================================
-// Brief   : MIIS Server Example with RDF Support
+// Brief   : MIIS Server example with RDF support
 // Authors : Simao Reis <sreis@av.it.pt>
 //------------------------------------------------------------------------------
 // ODTONE - Open Dot Twenty One
 //
-// Copyright (C) 2009-2011 Universidade Aveiro
-// Copyright (C) 2009-2011 Instituto de Telecomunicações - Pólo Aveiro
+// Copyright (C) 2009-2012 Universidade Aveiro
+// Copyright (C) 2009-2012 Instituto de Telecomunicações - Pólo Aveiro
 //
 // This software is distributed under a license. The full license
 // agreement can be found in the file LICENSE in this distribution.
--- lib/odtone/sap/user.cpp
+++ lib/odtone/sap/user.cpp
@@ -1,11 +1,11 @@
-//=============================================================================
+//==============================================================================
 // Brief   : MIH User SAP IO Service
 // Authors : Bruno Santos <bsantos@av.it.pt>
 //------------------------------------------------------------------------------
 // ODTONE - Open Dot Twenty One
 //
-// Copyright (C) 2009-2011 Universidade Aveiro
-// Copyright (C) 2009-2011 Instituto de Telecomunicações - Pólo Aveiro
+// Copyright (C) 2009-2012 Universidade Aveiro
+// Copyright (C) 2009-2012 Instituto de Telecomunicações - Pólo Aveiro
 //
 // This software is distributed under a license. The full license
 // agreement can be found in the file LICENSE in this distribution.
