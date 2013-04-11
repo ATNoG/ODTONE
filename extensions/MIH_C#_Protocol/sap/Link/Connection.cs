3d299380d9f91106d4238df983cfc17beb190168
Carlos Guimarães <cguimaraes@av.it.pt>
2013-04-11 09:41:53 +0100

    Added C# SAP libraries.

---
+++ extensions/MIH_C#_Protocol/sap/Link/Connection.cs
@@ -0,0 +1,159 @@
+﻿//==============================================================================
+// Brief   : Class representing a Connection.cs entity
+// Authors : Marcelo Génio <marcelog@ua.pt>
+//           Rui Fonseca <ruimfonseca@av.it.pt>
+//------------------------------------------------------------------------------
+// ODTONE - Open Dot Twenty One
+//
+// Copyright (C) 2009-2013 Universidade Aveiro
+// Copyright (C) 2009-2013 Instituto de Telecomunicações - Pólo Aveiro
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//==============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using ProtocolHeaderDefinition;
+using System.Collections;
+using System.Threading;
+
+namespace MIH_Connection.Link
+{
+    /// <summary>
+    /// Class responsible for providing tools to establish connections with
+    /// MIHF and to build and send UDP datagrams with MIH Protocol messsages.
+    /// </summary>
+    public class ConnectionHelper
+    {
+        /// <summary>
+        /// Link_sap IP Address. Default: 127.0.0.1.
+        /// </summary>
+        private IPAddress link_sap_address = IPAddress.Parse("127.0.0.1");
+        /// <summary>
+        /// Link_sap local port. Default: 1235
+        /// </summary>
+        private ushort link_sap_port = 1235;
+
+        /// <summary>
+        /// MIHF IP Address. Default: 127.0.0.1.
+        /// </summary>
+        private IPAddress mihf_address = IPAddress.Parse("127.0.0.1");
+        /// <summary>
+        /// MIHF local port. Default: 1025
+        /// </summary>
+        private ushort mihf_port = 1025;
+
+        /// <summary>
+        /// IP Address to bind the socket to.
+        /// </summary>
+        private IPAddress bind_address = IPAddress.Any;
+
+        /// <summary>
+        /// IPEndPoint of the MIHF instance.
+        /// </summary>
+        private IPEndPoint IPEnd;
+
+
+        /// <summary>
+        /// Packet Headers
+        /// </summary>
+        private byte[] built_packet;
+        private byte[] payLoad;
+        private UdpHeader udpPacket = new UdpHeader();
+        private ArrayList headerList = new ArrayList();
+        private Socket rawSocket = null;
+        private SocketOptionLevel socketLevel = SocketOptionLevel.IP;
+
+
+        /// <summary>
+        /// ConnectionHelper constructor.
+        /// </summary>
+        /// <param name="linkSapPort">The LinkSap communication port.</param>
+        /// <param name="address">The IPAddress of this connection.</param>
+        public ConnectionHelper(ushort linkSapPort, IPAddress address)
+        {
+            this.link_sap_port = linkSapPort;
+            IPEnd = new IPEndPoint(address, mihf_port);
+        }
+
+
+        /// <summary>
+        /// Receive Method.
+        /// </summary>
+        /// <returns>Data in a byte array form.</returns>
+        public byte[] Receive()
+        {
+            using (UdpClient client = new UdpClient(link_sap_port))
+            {
+                byte[] rcvd = client.Receive(ref IPEnd);
+                BitArray bar = new BitArray(rcvd);
+                //LOGHERE
+                //Console.WriteLine("Received "+rcvd.Length +" bytes");
+                //if (Program.PACKET_LOG)
+                //{
+                //    Console.WriteLine("----------------");
+                //    StringBuilder sb = new StringBuilder();
+                //    foreach (byte b in rcvd)
+                //        Console.Write(b.ToString("X2") + "\t");
+                //    Console.WriteLine();
+                //    Console.WriteLine("----------------");
+                //}
+                return rcvd;
+            }
+        }
+
+
+        /// <summary>
+        /// Send method.
+        /// </summary>
+        /// <param name="m">Serialized MIH Protocol message.</param>
+        public void Send(byte[] m)
+        {
+            //if (Program.MESSAGE_DETAILS)
+            //{
+            //    Console.WriteLine("Sending message: ");
+            //    Connection.MIHProtocol.PacketReader.PrintMessage(m);
+            //}
+            do
+            {
+                try
+                {
+                    using (UdpClient client = new UdpClient(link_sap_port))
+                    {
+                        client.Connect(IPEnd);
+                        //LOGHERE
+                        //Console.WriteLine("Sending " + m.ByteValue.Length + " bytes ("+m.MIHHeader.MID.AID+")");
+                        //if (Program.PACKET_LOG)
+                        //{
+                        //    Console.WriteLine("----------------");
+                        //    StringBuilder sb = new StringBuilder();
+                        //    foreach (byte b in m.ByteValue)
+                        //        Console.Write(b.ToString("X2") + "\t");
+                        //    Console.WriteLine();
+                        //    Console.WriteLine("----------------");
+                        //}
+                        client.Send(m, m.Length);
+                        client.Close();
+                        return;
+                    }
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Retrying...");
+                    Console.WriteLine();
+                    Thread.Sleep(new Random().Next(10, 1000)); } //Port in use error (caused by muti-threading), retry
+            } while (true);
+        }
+
+
+    }
+}
---
+++ extensions/MIH_C#_Protocol/sap/Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+﻿//==============================================================================
+// Brief   : Class representing a AssemblyInfo.cs entity
+// Authors : Marcelo Génio <marcelog@ua.pt>
+//------------------------------------------------------------------------------
+// ODTONE - Open Dot Twenty One
+//
+// Copyright (C) 2009-2013 Universidade Aveiro
+// Copyright (C) 2009-2013 Instituto de Telecomunicações - Pólo Aveiro
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//==============================================================================
+
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MIH Connection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MIH Connection")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a86ef9f8-c446-418d-8791-711a6d2ce9cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
