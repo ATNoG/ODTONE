ba3747aa3f5a2ec7659e7873bd954b47830e4271
Carlos Guimarães <cguimaraes@av.it.pt>
2013-06-24 23:14:38 +0100

    Improved IPv4 and IPv6 encoding and decoding

--- lib/odtone/mih/types/address.cpp
+++ lib/odtone/mih/types/address.cpp
@@ -21,6 +21,7 @@
 
 #include <boost/tokenizer.hpp>
 #include <boost/foreach.hpp>
+#include <boost/asio.hpp>
 
 #include <sstream>
 #include <string>
@@ -68,25 +69,22 @@ void mac_addr::address(const octet_string& addr)
 octet_string ip_addr::address() const
 {
 	octet_string str;
-	if(_type == ip_addr::ipv4) {
-		std::vector<int> tmp(_addr.size());
 
-		for (size_t i = 0; i < _addr.size(); ++i) {
-			if(i != 0)
-				str += '.';
-			tmp[i] = _addr[i] & 0x000000FF;
-			std::stringstream temp;
-			temp << tmp[i];
-			str += temp.str();
-		}
+	if(_type == ip_addr::ipv4) {
+		boost::asio::ip::address_v4::bytes_type bytes;
+		for(size_t i = 0; i < bytes.size(); ++i)
+			bytes[i] = _addr[i];
+
+		boost::asio::ip::address_v4 addrv4(bytes);
+		str = addrv4.to_string();
+
 	} else if(_type == ip_addr::ipv6) {
-		for (size_t i = 0; i < _addr.size(); i += 2) {
-			str += to_string_hex(&_addr[i], 1);
-			str += to_string_hex(&_addr[i + 1], 1);
-			str += ':';
-		}
-		if (str.length())
-			str.erase(str.length() - 1);
+		boost::asio::ip::address_v6::bytes_type bytes;
+		for(size_t i = 0; i < bytes.size(); ++i)
+			bytes[i] = _addr[i];
+
+		boost::asio::ip::address_v6 addrv6(bytes);
+		str = addrv6.to_string();
 	}
 
 	return str;
@@ -94,41 +92,25 @@ octet_string ip_addr::address() const
 
 void ip_addr::address(const octet_string& addr)
 {
-	using namespace boost;
+	boost::asio::ip::address address = boost::asio::ip::address::from_string(addr);
 
-	if(_type == ip_addr::ipv4) {
-		char_separator<char> sep(".");
-		tokenizer< char_separator<char> > list_tokens(addr, sep);
+	if(address.is_v4()) {
+		_type = ip_addr::ipv4;
 
-		_addr.clear();
+		boost::asio::ip::address_v4 addrv4 = address.to_v4();
+		boost::asio::ip::address_v4::bytes_type bytes = addrv4.to_bytes();
+		for (size_t i = 0; i < bytes.size(); ++i)
+			_addr += bytes[i];
 
-		BOOST_FOREACH(mih::octet_string str, list_tokens)
-		{
-			std::istringstream buffer(str);
-			unsigned int value;
-			buffer >> value;
-			_addr += value;
-		}
-	} else if(_type == ip_addr::ipv6) {
-		char_separator<char> sep(":");
-		tokenizer< char_separator<char> > list_tokens(addr, sep);
 
-		_addr.clear();
+	} else if(address.is_v6()) {
+		_type = ip_addr::ipv6;
 
-		BOOST_FOREACH(mih::octet_string str, list_tokens)
-		{
-			uchar tmp[4];
-			tmp[0] = 0;
-			tmp[1] = 0;
-			tmp[2] = 0;
-			tmp[3] = 0;
+		boost::asio::ip::address_v6 addrv6 = address.to_v6();
+		boost::asio::ip::address_v6::bytes_type bytes = addrv6.to_bytes();
+		for (size_t i = 0; i < bytes.size(); ++i)
+			_addr += bytes[i];
 
-			for (size_t j = 0; j < str.length(); ++j)
-				tmp[j] = hex_to_int(str[j]);
-
-			_addr += char((tmp[0] << 4) | tmp[1]);
-			_addr += char((tmp[2] << 4) | tmp[3]);
-		}
 	}
 }
 
